3cdad2af82f38145e0d5f15d06a4f07abb52cdda
risano <muhammadrisano@gmail.com>
2020-10-24 22:54:16 +0700

    hello

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>hello HelloWorld</h1>
+    <h3>hsdfsdf</h3>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
